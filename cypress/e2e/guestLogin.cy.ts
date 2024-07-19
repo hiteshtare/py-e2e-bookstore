e7b71d4d115723c086e591fd36b0c6449dd76d77
Hitesh Tare <hiteshtare@gmail.com>
2024-07-19 12:02:34 +0530

    Added Test suite for Guest login flow till PayNow click

--- cypress/e2e/guestLogin.cy.ts
+++ cypress/e2e/guestLogin.cy.ts
@@ -11,9 +11,7 @@ describe("Guest Login", () => {
   });
 
   it("should have open Bookstore Page then select AOY and give 200 OK status", () => {
-    checkStatus200ForLink(
-      guestLoginPages.btnAOYproduct
-    );
+    checkStatus200ForLink(guestLoginPages.btnAOYproduct);
   });
 
   it("should open AOY Product page", () => {
@@ -27,7 +25,6 @@ describe("Guest Login", () => {
 
     checkIfElementExist(guestLoginPages.btnAddToCart);
   });
-
 
   it("should open Cart page", () => {
     cy.get(guestLoginPages.btnAOYproduct).click();
@@ -45,7 +42,7 @@ describe("Guest Login", () => {
     checkIfElementExist(guestLoginPages.btnCheckout);
   });
 
-  it("should open Checkout Page", () => {
+  it("should open Checkout page", () => {
     cy.get(guestLoginPages.btnAOYproduct).click();
 
     cy.get(guestLoginPages.btnAddToCart).click();
@@ -53,5 +50,45 @@ describe("Guest Login", () => {
     cy.get(guestLoginPages.btnCheckout).click();
 
     cy.url().should("include", guestLoginPages.linkCheckout);
+  });
+
+  it("Checkout page should have Pay Now button", () => {
+    cy.get(guestLoginPages.btnAOYproduct).click();
+
+    cy.get(guestLoginPages.btnAddToCart).click();
+
+    cy.get(guestLoginPages.btnCheckout).click();
+
+    checkIfElementExist(guestLoginPages.btnPayNow);
+  });
+
+  it("Checkout page should fire validation, if input fields are empty", () => {
+    cy.get(guestLoginPages.btnAOYproduct).click();
+
+    cy.get(guestLoginPages.btnAddToCart).click();
+
+    cy.get(guestLoginPages.btnCheckout).click();
+
+    cy.wait(1000).get(guestLoginPages.btnPayNow).click();
+  });
+
+  it.only("Checkout page should proceed further if form is valid", () => {
+    cy.get(guestLoginPages.btnAOYproduct).click();
+
+    cy.get(guestLoginPages.btnAddToCart).click();
+
+    cy.get(guestLoginPages.btnCheckout).click();
+
+    cy.get("#billing_email").type(Cypress.env("BILLING_EMAIL"));
+    cy.get("#billing_phone").type(Cypress.env("BILLING_PHONE"));
+    cy.get("#billing_first_name").type(Cypress.env("BILLING_FIRST_NAME"));
+    cy.get("#billing_last_name").type(Cypress.env("BILLING_LAST_NAME"));
+    cy.get("#billing_address_1").type(Cypress.env("BILLING_ADDR1"));
+    cy.get("#billing_city").type(Cypress.env("BILLING_CITY"));
+    //Set Dropdown value on Form
+    cy.get("#billing_state").select(Cypress.env("BILLING_STATE"), { force: true });
+    cy.get("#billing_postcode").type(Cypress.env("BILLING_POSTCODE"));
+
+    cy.wait(1000).get(guestLoginPages.btnPayNow).click();
   });
 });
--- cypress/e2e/pages/guestLogin.page.ts
+++ cypress/e2e/pages/guestLogin.page.ts
@@ -5,18 +5,22 @@ import { TEST_CONFIG } from "../config";
 import { captureSreenshot } from "../util/common.util";
 
 export class GuestLoginPages {
-
   //AOY product
-  btnAOYproduct = '.jet-listing-dynamic-post-177392 > .elementor > .elementor-section > .elementor-container > .elementor-column > .elementor-widget-wrap > .elementor-element-49c9592 > .elementor-widget-container > .elementor-heading-title > a';
-  linkAOYproduct = 'product/autobiography-of-a-yogi';
-  
+  btnAOYproduct =
+    ".jet-listing-dynamic-post-177392 > .elementor > .elementor-section > .elementor-container > .elementor-column > .elementor-widget-wrap > .elementor-element-49c9592 > .elementor-widget-container > .elementor-heading-title > a";
+  linkAOYproduct = "product/autobiography-of-a-yogi";
+
   //Cart
-  btnAddToCart = '.elementor-add-to-cart > .variations_form > .single_variation_wrap > .woocommerce-variation-add-to-cart > .single_add_to_cart_button';
-  linkCart = 'cart';
+  btnAddToCart =
+    ".elementor-add-to-cart > .variations_form > .single_variation_wrap > .woocommerce-variation-add-to-cart > .single_add_to_cart_button";
+  linkCart = "cart";
 
   //Checkout
- btnCheckout = '.checkout-button';
- linkCheckout = 'checkout';
+  btnCheckout = ".checkout-button";
+  linkCheckout = "checkout";
+
+  //PayNow
+  btnPayNow = "#place_order";
 
   validateNewlinkAndStatusCode200(selector: string) {
     captureSreenshot();
